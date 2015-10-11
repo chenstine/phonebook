192d129cca0598cb25bb16bbdb837dac0007674b
ncbci <chenstine@yahoo.com.tw>
2015-10-11 23:46:20 +0800

    using hash function to reduce cache-misses

---
+++ phonebook_opt_hash.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "phonebook_opt_hash.h"
+
+hashTable *createHashTable(int tableSize)
+{
+    hashTable *ht;
+    ht = (hashTable *) malloc(sizeof(hashTable));
+    ht->buckets = (entry **) calloc(tableSize, sizeof(entry *));
+    ht->tableSize = tableSize;
+    return ht;
+}
+
+static inline uint32_t hash(char *key, hashTable *ht)
+{
+    uint32_t hashVal = 0;
+    while (*key != '\0') {
+        hashVal = (hashVal << 5) + hashVal + *key++;
+    }
+
+    return hashVal % ht->tableSize;
+}
+
+entry *findName(char lastName[], hashTable *ht)
+{
+    uint32_t hashIndex = hash(lastName, ht);
+    entry *pHead = ht->buckets[hashIndex];
+
+    while (pHead != NULL) {
+        if (strcasecmp(lastName, pHead->lastName) == 0)
+            return pHead;
+        pHead = pHead->pNext;
+    }
+
+    return NULL;
+}
+
+void append(char lastName[], hashTable *ht)
+{
+    uint32_t hashIndex = hash(lastName, ht);
+    entry *e;
+    e = (entry *) malloc(sizeof(entry));
+
+    strcpy(e->lastName, lastName);
+    e->pNext = ht->buckets[hashIndex];
+    ht->buckets[hashIndex] = e;
+}
---
+++ phonebook_opt_hash.h
@@ -0,0 +1,36 @@
+#ifndef _PHONEBOOK_OPT_HASH_H
+#define _PHONEBOOK_OPT_HASH_H
+
+#define MAX_LAST_NAME_SIZE 16
+
+/* original version */
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    char firstName[16];
+    char email[16];
+    char phone[10];
+    char cell[10];
+    char addr1[16];
+    char addr2[16];
+    char city[16];
+    char state[2];
+    char zip[5];
+    struct __PHONE_BOOK_ENTRY *pNext;
+} entryDetail;
+
+typedef struct __LASTNAME {
+    char lastName[MAX_LAST_NAME_SIZE];
+    struct __LASTNAME *pNext;
+    entryDetail *detail;
+} entry;
+
+typedef struct __HASH_TABLE {
+    int tableSize;
+    entry **buckets;
+} hashTable;
+
+hashTable *createHashTable(int tableSize);
+entry *findName(char lastname[], hashTable *ht);
+void append(char lastName[], hashTable *ht);
+
+#endif
